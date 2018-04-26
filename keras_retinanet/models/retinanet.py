d0fcb52a41c151c84d677be67b4defb3faf19243
Dmitry Zaika <dmitry.zaika@gmail.com>
2018-04-26 21:03:18 +0300

    Delete P7 feature pyramid level

--- keras_retinanet/models/retinanet.py
+++ keras_retinanet/models/retinanet.py
@@ -165,11 +165,7 @@ def __create_pyramid_features(C2, C3, C4, C5, feature_size=256):
     # "P6 is obtained via a 3x3 stride-2 conv on C5"
     P6 = keras.layers.Conv2D(feature_size, kernel_size=3, strides=2, padding='same', name='P6')(C5)
 
-    # "P7 is computed by applying ReLU followed by a 3x3 stride-2 conv on P6"
-    P7 = keras.layers.Activation('relu', name='C6_relu')(P6)
-    P7 = keras.layers.Conv2D(feature_size, kernel_size=3, strides=2, padding='same', name='P7')(P7)
-
-    return [P2, P3, P4, P5, P6, P7]
+    return [P2, P3, P4, P5, P6]
 
 
 class AnchorParameters:
@@ -194,8 +190,8 @@ class AnchorParameters:
 The default anchor parameters.
 """
 AnchorParameters.default = AnchorParameters(
-    sizes   = [16, 32, 64, 128, 256, 512],
-    strides = [4, 8, 16, 32, 64, 128],
+    sizes   = [16, 32, 64, 128, 256],
+    strides = [4, 8, 16, 32, 64],
     ratios  = np.array([0.5, 1, 2], keras.backend.floatx()),
     scales  = np.array([2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)], keras.backend.floatx()),
 )
@@ -351,7 +347,7 @@ def retinanet_bbox(
         model = retinanet(num_anchors=anchor_parameters.num_anchors(), **kwargs)
 
     # compute the anchors
-    features = [model.get_layer(name).output for name in ['P2','P3', 'P4', 'P5', 'P6', 'P7']]
+    features = [model.get_layer(name).output for name in ['P2', 'P3', 'P4', 'P5', 'P6']]
     anchors  = __build_anchors(anchor_parameters, features)
 
     # we expect the anchors, regression and classification values as first output
